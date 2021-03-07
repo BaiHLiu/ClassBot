d4ff92959d02d002ace0b0c2b6a3988ba7866689
catop <601179193@qq.com>
2021-03-07 15:31:52 +0800

    update sql

--- conf/qqbot.sql
+++ conf/qqbot.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2021-02-14 22:58:00
--- 服务器版本： 5.6.49-log
+-- 生成日期： 2021-03-07 15:29:46
+-- 服务器版本： 5.6.50-log
 -- PHP 版本： 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -25,10 +25,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `imginfo`
+-- 表的结构 `CA_project`
 --
 
-CREATE TABLE `imginfo` (
+CREATE TABLE `CA_project` (
+  `id` int(5) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `date` date NOT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='班级事项提醒_项目信息';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `CA_submit`
+--
+
+CREATE TABLE `CA_submit` (
+  `sid` int(8) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `pid` int(11) NOT NULL COMMENT '项目id',
+  `stime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='班级事项提醒_提交记录';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `CA_userinfo`
+--
+
+CREATE TABLE `CA_userinfo` (
+  `uid` int(5) NOT NULL,
+  `user_id` varchar(12) NOT NULL COMMENT 'QQ号',
+  `user_name` varchar(20) NOT NULL,
+  `user_class` varchar(30) NOT NULL,
+  `alert_count` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='班级事项提醒_成员信息';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `CZ_imginfo`
+--
+
+CREATE TABLE `CZ_imginfo` (
   `imgid` int(8) NOT NULL,
   `file_name` varchar(50) NOT NULL COMMENT '图片文件名',
   `user_id` varchar(12) NOT NULL COMMENT '发图人qq号',
@@ -37,7 +77,7 @@ CREATE TABLE `imginfo` (
   `ocr_err_code` int(2) NOT NULL,
   `ocr_times` varchar(5) NOT NULL,
   `ocr_scores` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='重走长征路截图上传_图片信息';
 
 -- --------------------------------------------------------
 
@@ -53,16 +93,34 @@ CREATE TABLE `userinfo` (
   `last_cmd` varchar(50) NOT NULL COMMENT '最近一条命令',
   `last_upload_date` date NOT NULL,
   `last_upload_time` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='注册用户信息';
 
 --
 -- 转储表的索引
 --
 
 --
--- 表的索引 `imginfo`
+-- 表的索引 `CA_project`
 --
-ALTER TABLE `imginfo`
+ALTER TABLE `CA_project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `CA_submit`
+--
+ALTER TABLE `CA_submit`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- 表的索引 `CA_userinfo`
+--
+ALTER TABLE `CA_userinfo`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- 表的索引 `CZ_imginfo`
+--
+ALTER TABLE `CZ_imginfo`
   ADD PRIMARY KEY (`imgid`);
 
 --
@@ -76,9 +134,27 @@ ALTER TABLE `userinfo`
 --
 
 --
--- 使用表AUTO_INCREMENT `imginfo`
+-- 使用表AUTO_INCREMENT `CA_project`
 --
-ALTER TABLE `imginfo`
+ALTER TABLE `CA_project`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `CA_submit`
+--
+ALTER TABLE `CA_submit`
+  MODIFY `sid` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `CA_userinfo`
+--
+ALTER TABLE `CA_userinfo`
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `CZ_imginfo`
+--
+ALTER TABLE `CZ_imginfo`
   MODIFY `imgid` int(8) NOT NULL AUTO_INCREMENT;
 
 --
